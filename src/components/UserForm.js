a764c7b7a65e6c9064bc28d53944a632253e4b30
saananokela <smnokela@gmail.com>
2024-08-11 18:59:29 +0300

    Updated UserForm component and added StartJumping button.

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,38 +1,60 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import '../App.css';
 
 const UserForm = () => {
   return (
-      <Container className="pt-4">
-        <Row className="justify-content-center">
-          <Col md={8}>
+      <Container>
             <Form className="userform pt-4">
-              <h5>Pomppijan valinta</h5>
-              <p>Kirjoita nimesi tai valitse nimesi alla olevasta listasta.</p>
+              <h5 className="fw-bolder pb-3" >Pomppijan valinta</h5>
               <Row className="justify-content-center">
-                <Col md={4} >
-                <Form.Label>Uusi pomppija</Form.Label>
-                  <Form.Control size="sm" placeholder="Nimi tai nimimerkki" />
-                </Col>
-                <Col  md={4} >
-                <Form.Label>Vanha pomppija</Form.Label>
+                <Col  md={3} >
+                <Form.Label>Valitse nimesi alla olevasta listasta:</Form.Label>
                   <Form.Select size="sm" >
-                  <option>Valitse nimesi:</option>
+                    <option></option>
                     <option value="1">Inkku</option>
                     <option value="2">Marsu</option>
-                    <option value="3">äippä</option>
                   </Form.Select>
                 </Col>
               </Row>
+              <Row className="justify-content-center mt-3">
+                <Col md={3}>
+                  <Button className="start-button" >
+                    Aloita hyppiminen
+                  </Button>
+                </Col>
+              </Row>
             </Form>
-          </Col>
-        </Row>
       </Container>
   );
 }
 
 export default UserForm;
+
+
+// {/* <Container>
+// {/* <Row className="justify-content-center"> */}
+//   {/* <Col md={8}> */}
+//     <Form className="userform pt-4">
+//       <h5 className="fw-bolder pb-3" >Pomppijan valinta</h5>
+//       {/* <p>Valitse nimesi alla olevasta listasta.</p> */}
+//       <Row className="justify-content-center">
+//         {/* <Col md={4} >
+//         <Form.Label>Uusi pomppija</Form.Label>
+//           <Form.Control size="sm" placeholder="Nimi tai nimimerkki" />
+//         </Col> */}
+//         <Col  md={3} >
+//         <Form.Label>Valitse nimesi alla olevasta listasta:</Form.Label>
+//           <Form.Select size="sm" >
+//             <option></option>
+//             <option value="1">Inkku</option>
+//             <option value="2">Marsu</option>
+//           </Form.Select>
+//         </Col>
+//       </Row>
+//     </Form>
+//   {/* </Col> */}
+// {/* </Row> */}
+// </Container>
+// );
+// } */}
