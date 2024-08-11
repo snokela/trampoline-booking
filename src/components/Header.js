4684521e7000771ed39ff78b0e3703f623fa4492
saananokela <smnokela@gmail.com>
2024-08-11 11:55:36 +0300

    Added new favicon, removed old favicon, and updated app name in index.html. Added Header-component.

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "react-bootstrap/Container";
+import { Navbar } from "react-bootstrap/Navbar";
+
+const Header = () => {
+
+}
+
+export default Header;
