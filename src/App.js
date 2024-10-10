ef4880e4b26918922ff42f05e3d49660843b6fe5
saananokela <smnokela@gmail.com>
2024-10-10 13:49:50 +0300

    Moved state and jumping logic from App.js to HomePage.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Header from './components/Header';
-import GetJumpers from './components/GetJumpers';
+// import GetJumpers from './components/GetJumpers';
 import React, { useContext, useEffect, useRef, useState } from 'react';
-import getCurrentTimeStamp from './utilis/getCurrentTimeStamp';
-import updateHistory from './utilis/upDateHistory';
-import useLocalStorage from './utilis/useLocalStorage';
+// import getCurrentTimeStamp from './utilis/getCurrentTimeStamp';
+// import updateHistory from './utilis/upDateHistory';
+// import useLocalStorage from './utilis/useLocalStorage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import JumpHistoryPage from './components/JumpHistoryPage';
@@ -13,72 +13,72 @@ import Footer from './components/Footer';
 import { JumperProvider } from './contexts/JumperDataContext';
 
 function App() {
-  const [history, setHistory] = useLocalStorage('jumpingData', []);
-  const [jumpers, setJumpers] = useState(GetJumpers());
-  const [currentJumper, setCurrentJumper] = useState(null);
-  const [jumpTime, setJumpTime] = useState(0);
-  const [isJumping, setIsJumping] = useState(false);
-  const [jumpStopped, setJumpStopped] = useState(false);
-  const intervalRef = useRef(null);
-  const startDateTimeRef = useRef(null);
+  // const [history, setHistory] = useLocalStorage('jumpingData', []);
+  // const [jumpers, setJumpers] = useState(GetJumpers());
+  // const [currentJumper, setCurrentJumper] = useState(null);
+  // const [jumpTime, setJumpTime] = useState(0);
+  // const [isJumping, setIsJumping] = useState(false);
+  // const [jumpStopped, setJumpStopped] = useState(false);
+  // const intervalRef = useRef(null);
+  // const startDateTimeRef = useRef(null);
 
   // haetaan pomppijat 'useEffect' -hookilla ja asetetaan se 'jumpers' tilaan
-  useEffect(() => {
-    const jumpersList = GetJumpers();
-    console.log('Asetetaan hyppijälista:', jumpersList);
-    setJumpers(jumpersList);
-    console.log('!!!Jumpers on setJumpersin jälkeen: ' + JSON.stringify(jumpers))
-  }, []);
+  // useEffect(() => {
+  //   const jumpersList = GetJumpers();
+  //   console.log('Asetetaan hyppijälista:', jumpersList);
+  //   setJumpers(jumpersList);
+  //   console.log('!!!Jumpers on setJumpersin jälkeen: ' + JSON.stringify(jumpers))
+  // }, []);
 
-  // Ajastimen hallinta 'useEffect'-hookilla
-  useEffect(() => {
-    if (isJumping) {
-      console.log('Käynnistetään ajastin')
-      intervalRef.current = setInterval(() => {
-        setJumpTime(prevTime => prevTime + 1);
-      }, 1000);
-    }
-    // cleanup -functio: ajastin pysäytetään jos isjumping muuttuu tai komponentti poistetaan
-    return () => {
-      clearInterval(intervalRef.current);
-      intervalRef.current = null //nollataan, ettei jää vanhoja ajastimia
-    };
-  }, [isJumping]);
+  // // Ajastimen hallinta 'useEffect'-hookilla
+  // useEffect(() => {
+  //   if (isJumping) {
+  //     console.log('Käynnistetään ajastin')
+  //     intervalRef.current = setInterval(() => {
+  //       setJumpTime(prevTime => prevTime + 1);
+  //     }, 1000);
+  //   }
+  //   // cleanup -functio: ajastin pysäytetään jos isjumping muuttuu tai komponentti poistetaan
+  //   return () => {
+  //     clearInterval(intervalRef.current);
+  //     intervalRef.current = null //nollataan, ettei jää vanhoja ajastimia
+  //   };
+  // }, [isJumping]);
 
-  // funktio, joka käynnistää pomppimisen
-  const handleStartJumping = (jumperId) => {
-    const selectedJumper = jumpers.find(jumper => jumper.id === jumperId);
-    console.log('Valittu pomppija on: ' + JSON.stringify(selectedJumper))
-    startDateTimeRef.current = getCurrentTimeStamp();
-    setCurrentJumper(selectedJumper)
-    setIsJumping(true);
-    setJumpTime(0);
-    setJumpStopped(false);
-  }
+  // // funktio, joka käynnistää pomppimisen
+  // const handleStartJumping = (jumperId) => {
+  //   const selectedJumper = jumpers.find(jumper => jumper.id === jumperId);
+  //   console.log('Valittu pomppija on: ' + JSON.stringify(selectedJumper))
+  //   startDateTimeRef.current = getCurrentTimeStamp();
+  //   setCurrentJumper(selectedJumper)
+  //   setIsJumping(true);
+  //   setJumpTime(0);
+  //   setJumpStopped(false);
+  // }
 
-  // funktio joka lopettaa pomppimisen
-  const handleStopJumping = () => {
-    setIsJumping(false);
-    setJumpStopped(true);
+  // // funktio joka lopettaa pomppimisen
+  // const handleStopJumping = () => {
+  //   setIsJumping(false);
+  //   setJumpStopped(true);
 
-    const endDateTime = getCurrentTimeStamp();
+  //   const endDateTime = getCurrentTimeStamp();
 
-      // tämä objekti tallennetaan contextiin
-    const jumpingData = {
-      name: currentJumper.name,
-      timeSecs: jumpTime,
-      startDate: startDateTimeRef.current.date,
-      startTime: startDateTimeRef.current,
-      endDate: endDateTime.date,
-      endTime: endDateTime
-    };
+  //     // tämä objekti tallennetaan contextiin
+  //   const jumpingData = {
+  //     name: currentJumper.name,
+  //     timeSecs: jumpTime,
+  //     startDate: startDateTimeRef.current.date,
+  //     startTime: startDateTimeRef.current,
+  //     endDate: endDateTime.date,
+  //     endTime: endDateTime
+  //   };
 
-    //päivitetään historia ja tallennetaan localstorageen
-    setHistory(prevHistory => updateHistory(prevHistory, jumpingData));
+  //   //päivitetään historia ja tallennetaan localstorageen
+  //   setHistory(prevHistory => updateHistory(prevHistory, jumpingData));
 
-    //tyhjennetään startDateTimeRef
-    startDateTimeRef.current = null;
-  }
+  //   //tyhjennetään startDateTimeRef
+  //   startDateTimeRef.current = null;
+  // }
 
   return (
     <JumperProvider>
@@ -91,13 +91,13 @@ function App() {
                 path="/"
                 element={
                   <HomePage
-                    jumpers={jumpers}
-                    isJumping={isJumping}
-                    onStartJumping={handleStartJumping}
-                    currentJumper={currentJumper}
-                    onStopJumping={handleStopJumping}
-                    jumpTime={jumpTime}
-                    jumpStopped={jumpStopped}
+                    // jumpers={jumpers}
+                    // isJumping={isJumping}
+                    // onStartJumping={handleStartJumping}
+                    // currentJumper={currentJumper}
+                    // onStopJumping={handleStopJumping}
+                    // jumpTime={jumpTime}
+                    // jumpStopped={jumpStopped}
                   />
                 }
               />
@@ -105,7 +105,7 @@ function App() {
                 path='/jumping-history'
                 element={
                   <JumpHistoryPage
-                    history={history}
+                    // history={history}
                   />
                 }
               />
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,93 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import '../App.css';
 import UserForm from '../components/UserForm';
 import CurrentJumper from '../components/CurrentJumper';
+import getCurrentTimeStamp from '../utilis/getCurrentTimeStamp';
 import { JumperDataContext } from "../contexts/JumperDataContext";
+import GetJumpers from "./GetJumpers";
 
-const HomePage = ({ jumpers, isJumping,  onStartJumping, currentJumper, onStopJumping, jumpTime, jumpStopped }) => {
+const HomePage = () => {
 
   //  tuodaan contextista jumperdata
   const { jumpersData, setJumpersData } = useContext(JumperDataContext);
+  // tilanhallinta
+  const [jumpers, setJumpers] = useState(GetJumpers);
+  const [currentJumper, setCurrentJumper] = useState(null);
+  const [jumpTime, setJumpTime] = useState(0);
+  const [isJumping, setIsJumping] = useState(false);
+  const [jumpStopped, setJumpStopped] = useState(false);
+  const intervalRef = useRef(null);
+  const startDateTimeRef = useRef(null);
 
-  console.log('Jumpers data: ' + JSON.stringify(jumpers))
+  // useEffect(() => {
+  //   // haetaan kaikki hyppijät tällähetkellä kahdesta kovakoodatusta pomppijasta listaan
+  //   const jumpersList = GetJumpers();
+  //   console.log('Asetetaan hyppijälista:', jumpersList);
+  //   setJumpers(jumpersList);
+  //   console.log('!!!Jumpers on setJumpersin jälkeen: ' + JSON.stringify(jumpers))
+  // }, []);
+
+  // Ajastimen hallinta 'useEffect'-hookilla
+  useEffect(() => {
+    if (isJumping) {
+      console.log('Käynnistetään ajastin')
+      intervalRef.current = setInterval(() => {
+        setJumpTime(prevTime => prevTime + 1);
+      }, 1000);
+    }
+    // cleanup -functio: ajastin pysäytetään jos isjumping muuttuu tai komponentti poistetaan
+    return () => {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null //nollataan, ettei jää vanhoja ajastimia
+    };
+  }, [isJumping]);
+
+  // funktio, joka käynnistää pomppimisen
+  const handleStartJumping = (jumperId) => {
+    const selectedJumper = jumpers.find(jumper => jumper.id === jumperId);
+    console.log('Valittu pomppija on: ' + JSON.stringify(selectedJumper))
+    startDateTimeRef.current = getCurrentTimeStamp();
+    setCurrentJumper(selectedJumper)
+    setIsJumping(true);
+    setJumpTime(0);
+    setJumpStopped(false);
+  }
+
+  // funktio joka lopettaa pomppimisen
+  const handleStopJumping = () => {
+    setIsJumping(false);
+    setJumpStopped(true);
+
+    const endDateTime = getCurrentTimeStamp();
+
+      // tämä objekti tallennetaan contextiin
+    const jumpingData = {
+      name: currentJumper.name,
+      timeSecs: jumpTime,
+      startDate: startDateTimeRef.current.date,
+      startTime: startDateTimeRef.current,
+      endDate: endDateTime.date,
+      endTime: endDateTime
+    };
+
+    //tallennetaan uusi pomppijadata contextiin
+    setJumpersData((prev) => [...prev, jumpingData])
+    // setHistory(prevHistory => updateHistory(prevHistory, jumpingData));
+
+    //tyhjennetään startDateTimeRef
+    startDateTimeRef.current = null;
+  }
+
   return (
     <div>
       <UserForm
         jumpers={jumpers}
         isJumping={isJumping}
-        onStartJumping={onStartJumping}
+        onStartJumping={handleStartJumping}
       />
       <CurrentJumper
         currentJumper={currentJumper}
-        onStopJumping={onStopJumping}
+        onStopJumping={handleStopJumping}
         jumpTime={jumpTime}
         jumpStopped={jumpStopped}
       />
